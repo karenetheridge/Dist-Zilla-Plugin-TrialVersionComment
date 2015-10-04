70ca8818ce2c75e5b73d899b5e09b8fd6570ffde
Karen Etheridge <ether@cpan.org>
2015-10-03 17:25:42 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/TrialVersionComment.pm
+++ lib/Dist/Zilla/Plugin/TrialVersionComment.pm
@@ -116,14 +116,6 @@ L<Module::Metadata> might make use of this in the future.
 
 =for Pod::Coverage munge_files
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-TrialVersionComment>
-(or L<bug-Dist-Zilla-Plugin-TrialVersionComment@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-TrialVersionComment@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 ACKNOWLEDGEMENTS
 
 =for stopwords xdg
