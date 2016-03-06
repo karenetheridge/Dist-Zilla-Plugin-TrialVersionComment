d6b1eb2d6ddc054fd54aaba105f7a6acdff2604c
Karen Etheridge <ether@cpan.org>
2016-03-05 20:21:50 -0800

    increment $VERSION after 0.005 release

--- lib/Dist/Zilla/Plugin/TrialVersionComment.pm
+++ lib/Dist/Zilla/Plugin/TrialVersionComment.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::TrialVersionComment;
 # KEYWORDS: plugin modules package version comment trial release
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.005';
+our $VERSION = '0.006';
 
 use Moose;
 with
